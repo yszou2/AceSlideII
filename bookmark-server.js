8d00a441aa20946fb24afd5cd578e5364d14285b
Yesheng Zou <yeshengzou@gmail.com>
2012-09-02 23:43:11 +0800

    添加书签需要的代码

---
+++ bookmark-server.js
@@ -0,0 +1,1 @@
+javascript: (function(){var ip=prompt('Server')||''; if(ip==''){return} var pass=prompt('Password')||''; var name=prompt('Name')||''; if(ip==''||pass==''||name==''){return} var n=document.createElement('script'); n.type='text/javascript'; n.src='http://' + ip + '/reg?ip=' + ip + '&pass=' + pass + '&name=' + name; document.getElementsByTagName('head')[0].appendChild(n); })();
