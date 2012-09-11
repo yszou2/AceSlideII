932d2caa91ddd81cf6ca5b52a160b792dab00786
Zou Yesheng <yeshengzou@sohu-inc.com>
2012-09-11 14:34:04 +0800

    vp规则更新

--- AceSlideII/bookmark.js
+++ AceSlideII/bookmark.js
@@ -18,3 +18,5 @@
 
 
 javascript:(function(){ if((typeof dojo) != 'undefined'){return} window.dojoConfig = { async: true , packages: [ {name: 'AceSlideII', location: 'http://localhost:8000/AceSlideII'} ] , deps: ['AceSlideII/init', 'dojo/dom-construct'] , callback: function(init, cstr){ init({width: 600}, cstr); } }; var n = document.createElement('script'); n.type='text/javascript'; n.src='http://ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojo/dojo.js'; document.getElementsByTagName('head')[0].appendChild(n); })();
+
+javascript:(function(){ if((typeof dojo) != 'undefined'){return} window.dojoConfig = { async: true , packages: [ {name: 'AceSlideII', location: 'http://zouyesheng.com/static/AceSlideII'} ] , deps: ['AceSlideII/init', 'dojo/dom-construct'] , callback: function(init, cstr){ init({width: 600}, cstr); } }; var n = document.createElement('script'); n.type='text/javascript'; n.src='http://ajax.googleapis.com/ajax/libs/dojo/1.8.0/dojo/dojo.js'; document.getElementsByTagName('head')[0].appendChild(n); })();
