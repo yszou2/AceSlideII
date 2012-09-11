83efc0ceb794ce6b6a70d659e9036a19e2cacb75
Yesheng Zou <yeshengzou@gmail.com>
2012-09-11 08:29:42 +0800

    更新vp2规则

--- AceSlideII/rules/vp2_chrs2.js
+++ AceSlideII/rules/vp2_chrs2.js
@@ -1,8 +1,20 @@
 //北欧女神战魂
 define(['require', 'dojo/query', 'dojo/dom-construct'], function(require, query, cstr){
-  var ready_html = function(html_list, callback){
-    console.log(html_list);
-    //callback();
+
+  var do_page = function(html){
+    (/<B>(.*?)<\/B>/).exec(html);
+    var name = RegExp.$1.replace(/·/g, '');
+
+    (/src="([^>]*?)" width="300"/).exec(html);
+    var img = RegExp.$1;
+
+    var node = cstr.create('div', {innerHTML: html});
+    var q = query('table', node)[1];
+    q = query('div', q);
+    q = q[q.length - 1];
+    var data = q.innerHTML;
+
+    return {name: name, img: img, data: data};
   }
 
 
@@ -12,17 +24,14 @@ define(['require', 'dojo/query', 'dojo/dom-construct'], function(require, query,
       href_list.push(item.href);
     });
 
-    var html_list = [];
     for(var i = 0, l = href_list.length; i < l; i++){
-      require(['dojo/text!' + href_list[0]], function(html){
-        html_list.push(cstr.create('div', {innerHTML: html}));
-        if(html_list.length == href_list.length){
-          ready_html(html_list, callback);
-        }
+      require(['dojo/text!' + href_list[i]], function(html){
+        do_page(html);
       });
     }
 
     //callback();
   }
+
   return rule;
 });
