ab4597535d082e468bb1032e11e74170b28ee1df
Yesheng Zou <yeshengzou@gmail.com>
2013-02-27 02:35:54 +0800

    添加webqq回复优化

--- rules/0rules.js
+++ rules/0rules.js
@@ -5,6 +5,7 @@ define([], function(){
     , [/^http:\/\/localhost.*$/, 'default']
     , [/^http:\/\/vp\.ffsky\.cn\/chrs2\.htm$/, 'vp2_chrs2']
     , [/^http:\/\/cwebmail\.mail\.163\.com.*$/, 'mail_163']
+    , [/^http:\/\/web\.qq\.com.*$/, 'web_qq']
     , [/^https?:\/\/www\.google\.com\/search.*$/, 'google_search']
     , [/^https?:\/\/www\.google\.com\.hk\/search.*$/, 'google_search']
   ];
